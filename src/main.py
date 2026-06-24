f35f5da6217d9dc97273d38c4dd3461202b27c85
gtc <159605112+grahametc@users.noreply.github.com>
2026-06-23 21:11:50 -0400

    hey

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import argparse, math
 
 
-def tst(input):
+def main(input, thresh, color, fill, text, text_color, con, con_color, con_thresh, txt_visuals, tv_color, invert, blur, blur_amt):
     cap = cv.VideoCapture(input)
 
     if cap.isOpened():
@@ -21,7 +21,6 @@ def tst(input):
     output = cv.VideoWriter("output.mp4", fourcc, FPS, (WIDTH, HEIGHT), isColor=True)
     history=defaultdict(tuple)
     frame_count=0
-    font = "neuropol.ttf"
     while True:
         ret, frame = cap.read()
         if not ret: break
@@ -32,18 +31,11 @@ def tst(input):
         _, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
         (all_contours, hierarchy) = cv.findContours(binary, cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
         if len(all_contours)>0:
-            threshold = 50
+            threshold = thresh
             contours = [x for x in all_contours if cv.contourArea(x) > threshold]
-            srt = sorted(contours, key = cv.contourArea)
-            if len(srt) > 0:
-                lx,ly,lw,lh = cv.boundingRect(srt[0])
-            blobs = 0
+            srt = sorted(contours, key = cv.contourArea) 
             id = 0
-            prev = (WIDTH // 2, HEIGHT // 2)   #middle of frame
-            #prev = (lx, ly)     
-            #               #lines originate from largest blob
             seen = set()
-            #print(history)
             max_length = len(contours)
             if len(history) == 0 or len(contours) > max(history):
                 max_length = len(contours)
@@ -51,68 +43,41 @@ def tst(input):
                 max_length=max(history)
             xs=[0] * max_length
             ys=[0] * max_length
-            #print(len(contours))
-            #print(xs)
-            #print(ys)
-            blob_data = []
+            blob_data = []   
             for contour in contours:
-                #print(contour)
                 area = cv.contourArea(contour)
-                if area >= 1500 and area <= 2000:
-                    cv.drawContours(frame, [contour], 0, (255, 255, 255), 1)    #-1 to fill
-                    #opaque_overlay(frame, contour, (0,255,255))
-                elif area <= 3000:
-                    blobs+=1
-                    x,y,w,h = cv.boundingRect(contour)
-                    font_size = 1
-                    #cv.putText(frame, str(blobs), (x, y+20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), font_size)
-                    distance = math.dist((x, y), prev)
-                    #print(f"distance: {distance}")
-                    #draw_trajectory(frame, history, id)
-                    #if (distance  > WIDTH / 4 or distance  > HEIGHT / 4 ):  
-                    #    cv.line(frame, prev, (x, y), (255, 255, 255), 1)
-                    #draw_trajectory(frame, history, id)
-                    #prev = (x, y)
-                    closest = find_closest(history, x, y, seen)
-                    if not history[id] or closest == -1:
-                        id +=1 
-                    else:
-                        id = closest
-                    seen.add(id)
-                    #print(f"id: {id} ")
-                    #print(f"xs: {len(xs)}")
-                    if id > len(xs): 
-                       xs.append(0)
-                       ys.append(0)
-                    #print(f"id: {id}")
-                    #print(f"xs: {len(xs)}")
-                    xs[id-1] = x + (w // 2)
-                    ys[id-1] = y + (h // 2)
-                   # print(f"seen: {seen}")
-                    #print(f'id:{id}')
-                    #overlay = frame.copy()
-                    #invert_blob_color(frame, contour)
-                    #blur_blob(frame, contour, (20, 20))
-                    cv.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),1)    #-1 for fill
-                    cv.putText(frame, str(id), ((x+w), y+(h//2)), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), font_size)
-                    #alpha = 1
-                    #cv.addWeighted(overlay, alpha, frame, 1-alpha, 0, frame)
-                    history.setdefault(id, ([]))
-                    history.setdefault(id, ([])).append((x, y+(h//2))) 
-                    #draw_trajectory(frame, history, id)
-                    data = f"x:{x} y:{y} id:{id}"
-                    blob_data.append(data) 
-            #connect_blobs(frame, contours, xs, ys)  
-            #text_visuals(frame, blob_data, (0, 0, 255), HEIGHT)
-            #blur_blob(frame, contour, (50,50))
-            #connect_blobs(frame, contours, xs, ys, (255, 255, 255))     
-            output.write(frame)
-        else:
-            history = defaultdict(tuple)
-    cv.destroyAllWindows() 
-        
-    cap.release()
-    output.release()
+                x,y,w,h = cv.boundingRect(contour)
+                closest = find_closest(history, x, y, seen)
+                if not history[id] or closest == -1:
+                    id +=1 
+                else:
+                    id = closest
+                seen.add(id)
+                if id > len(xs): 
+                    xs.append(0)
+                    ys.append(0)
+                cv.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),1)    #-1 for fill
+                if text:
+                     cv.putText(frame, str(id), ((x+w), y+(h//2)), cv.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
+                history.setdefault(id, ([]))
+                history.setdefault(id, ([])).append((x, y+(h//2)))
+                if invert:
+                    invert_blob_color(frame, contour)
+                elif blur:
+                    blur_blob(frame, contour, blur_amt)
+                data = f"x:{x} y:{y} id:{id}"
+                blob_data.append(data)    
+            if con:
+                connect_blobs(frame, contours, xs, ys, con_color)
+            if txt_visuals:
+                text_visuals(frame, blob_data, text_color, HEIGHT)
+        output.write(frame)
+
+
+
+                
+
+
 
 def draw_trajectory(frame, history, id):
     points = history.get(id)
@@ -211,4 +176,4 @@ if __name__=="__main__":
    parser = argparse.ArgumentParser()
    parser.add_argument("-input", "-input", required = True)
    args = parser.parse_args()
-   tst(args.input)
+   main(args.input, 0, (255,255,255), 1, True, (255, 255, 255), True, (255,255,255), 200, False, (255, 255, 255), False, False, (10, 10))
